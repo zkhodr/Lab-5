7036889b1570eda55d3ae201cb75a6b8e1909e6a
Rich Hildred <rhildred@gmail.com>
2022-01-17 17:37:49 -0500

    add module

---
+++ functions.js
@@ -0,0 +1,6 @@
+function hello(){
+    return "hello";
+}
+
+
+export {hello}
--- spec/introduction_spec.js
+++ spec/introduction_spec.js
@@ -1,8 +1,9 @@
-
+import { hello } from '../functions.js'
 
 describe("A suite", function() {
     it("contains spec with an expectation", function() {
-      expect(true).toBe(false);
+      let sHello = hello();
+      expect(sHello).toBe("hello");
     });
   });
   
